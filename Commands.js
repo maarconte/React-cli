9c83e05985b0292e6e4c206b93e8fe5efc9e733a
Thomas Guillouët <thomas.guillouet@myconnectedcompany.com>
2019-09-27 13:05:18 +0200

    Import dans le composant

--- Commands.js
+++ Commands.js
@@ -12,7 +12,7 @@ class Commands {
     }
 
     create() {
-        let ReactTemplate = `import React, { Component } from 'react';\n\nexport default class ${this.program.create} extends Component {\n\tconstructor(props) {\n\t\tsuper(props);\n\t}\n\n\trender() {\n\t\treturn (\n\t\t\t<div>\n\t\t\t<h1>Hello world !</h1>\n\t\t\t</div>\n\t\t);\n\t}\n}`;
+        let ReactTemplate = `import React, { Component } from 'react';\nimport './_${this.program.create}.${style.ext}';\n\nexport default class ${this.program.create} extends Component {\n\tconstructor(props) {\n\t\tsuper(props);\n\t}\n\n\trender() {\n\t\treturn (\n\t\t\t<div>\n\t\t\t<h1>Hello world !</h1>\n\t\t\t</div>\n\t\t);\n\t}\n}`;
         let ReactTestTemplate = `import React from 'react';\nimport ReactDOM from 'react-dom';\nimport ${this.program.create} from './${this.program.create}';\n\nit('renders without crashing', () => {\nconst div = document.createElement('div');\nReactDOM.render(<${this.program.create} />, div);\nReactDOM.unmountComponentAtNode(div);\n});`;
 
         mkdirp(`./src/${componentPath}/${this.program.create}`, (e) => {
@@ -25,7 +25,7 @@ class Commands {
 
             // Write styles clauses (make a difference with CSS and SASS or SCSS files)
             if (style.ext === "sass" || style.ext === "scss") {
-                fs.appendFileSync(`src/${style.path}_imports.${style.ext}`, `@import './src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}';\n`);
+                // fs.appendFileSync(`src/${style.path}_imports.${style.ext}`, `@import './src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}';\n`);
                 fs.writeFileSync(`src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}`, '');
             } else {
                 fs.writeFileSync(`src/${componentPath}/${this.program.create}/${this.program.create}.css`, '');
