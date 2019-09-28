069fdc504faa1005d8a88fe64e5d315fd69ef632
Thomas Guillouët <thomas.guillouet@outlook.com>
2019-09-28 10:37:33 +0200

    Ajout de la config

--- Commands.js
+++ Commands.js
@@ -4,7 +4,8 @@ var mkdirp = require('mkdirp');
 
 let config = require('./config.json');
 let componentPath = config.path;
-let style = config.style;
+let stylePath = config.stylePath;
+let styleExt = config.styleExt;
 
 class Commands {
     constructor(program) {
@@ -12,7 +13,7 @@ class Commands {
     }
 
     create() {
-        let ReactTemplate = `import React, { Component } from 'react';\nimport './_${this.program.create}.${style.ext}';\n\nexport default class ${this.program.create} extends Component {\n\tconstructor(props) {\n\t\tsuper(props);\n\t}\n\n\trender() {\n\t\treturn (\n\t\t\t<div>\n\t\t\t<h1>Hello world !</h1>\n\t\t\t</div>\n\t\t);\n\t}\n}`;
+        let ReactTemplate = `import React, { Component } from 'react';\n\nexport default class ${this.program.create} extends Component {\n\tconstructor(props) {\n\t\tsuper(props);\n\t}\n\n\trender() {\n\t\treturn (\n\t\t\t<div>\n\t\t\t<h1>Hello world !</h1>\n\t\t\t</div>\n\t\t);\n\t}\n}`;
         let ReactTestTemplate = `import React from 'react';\nimport ReactDOM from 'react-dom';\nimport ${this.program.create} from './${this.program.create}';\n\nit('renders without crashing', () => {\nconst div = document.createElement('div');\nReactDOM.render(<${this.program.create} />, div);\nReactDOM.unmountComponentAtNode(div);\n});`;
 
         mkdirp(`./src/${componentPath}/${this.program.create}`, (e) => {
@@ -25,8 +26,8 @@ class Commands {
 
             // Write styles clauses (make a difference with CSS and SASS or SCSS files)
             if (style.ext === "sass" || style.ext === "scss") {
-                // fs.appendFileSync(`src/${style.path}_imports.${style.ext}`, `@import './src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}';\n`);
-                fs.writeFileSync(`src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}`, '');
+                fs.appendFileSync(`src/${stylePath}_imports.${styleExt}`, `@import './src/${componentPath}/${this.program.create}/_${this.program.create}.${style.ext}';\n`);
+                fs.writeFileSync(`src/${componentPath}/${this.program.create}/_${this.program.create}.${styleExt}`, '');
             } else {
                 fs.writeFileSync(`src/${componentPath}/${this.program.create}/${this.program.create}.css`, '');
             }
@@ -34,6 +35,24 @@ class Commands {
             // Write template for tests
             fs.writeFileSync(`./src/${componentPath}/${this.program.create}/${this.program.create}.test.js`, ReactTestTemplate);
         }, 1000);
+    }
+
+    config() {
+        const args = this.program.config.split(',');
+        // Parser le config.json
+        const configFile = JSON.parse(fs.readFileSync('./config.json').toString());
+        
+        args.forEach(arg => {
+            const keyVal = arg.split('='); // Récupération de la valeur
+
+            // Modifier la valeur à la clée correspondante
+            let key = keyVal[0];
+            const value = keyVal[1];
+            configFile[key] = value;
+        });
+
+        // Réécrire le fichier de config
+        fs.writeFileSync('./config.json', JSON.stringify(configFile));
     }
 }
 module.exports = Commands;
--- cli.js
+++ cli.js
@@ -5,11 +5,17 @@ let Commands = require('./Commands.js'); // All commands actions
 
 /* Define commands */
 program
-  .option('-c|--create [value]', 'Create a react component')
+  .option('-c, --create [value]', 'Create a react component')
+  .option('--config [value]', 'Update the config file')
   .parse(process.argv);
 
 /* Define actions of each command */
 if (program.create !== true && typeof program.create !== 'undefined') {
-  let commands = new Commands(program);
+  const commands = new Commands(program);
   commands.create();
+}
+
+if (program.config !== true && typeof program.config !== 'undefined') {
+  const commands = new Commands(program);
+  commands.config();
 }
